99707443409beef0879b66ba065bca56d73ecc87
HangHang <dhkdwlsgod@naver.com>
2022-09-06 14:02:53 +0900

    feat: 몽고db

--- sub2/backend/api/models.py
+++ sub2/backend/api/models.py
@@ -18,19 +18,19 @@ class Store(models.Model):
         return self.category.split("|") if self.category else []
 
 
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
+# class Blog(models.Model):
+#     name = models.CharField(max_length=100)
+#     tagline = models.TextField()
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
-class Entry(models.Model):
-    blog = models.EmbeddedField(
-        model_container=Blog,
-    )
+# class Entry(models.Model):
+#     blog = models.EmbeddedField(
+#         model_container=Blog,
+#     )
     
-    headline = models.CharField(max_length=255)
+#     headline = models.CharField(max_length=255)
 
 # e = Entry()
 # e.blog = {
