4b36b20458903744acfad962cb2207cc6284439d
mingyu <mingyu1218@naver.com>
2022-09-12 19:28:49 +0900

    chore: ButtonWrapper button 속성 변경

--- Frontend/src/components/styled/Wrapper.js
+++ Frontend/src/components/styled/Wrapper.js
@@ -1,84 +1,85 @@
 import styled from "styled-components";
 
 export const BaseFlexWrapper = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const BaseFlexColWrapper = styled(BaseFlexWrapper)`
-	flex-direction: column;
+  flex-direction: column;
 `;
 
 export const Wrapper = styled(BaseFlexWrapper)`
-	background-color: ${({ color }) => color};
-	width: 100vw;
-	min-height: 100vh;
-	align-items: ${({ alignItems }) => alignItems};
+  background-color: ${({ color }) => color};
+  width: 100vw;
+  min-height: 100vh;
+  align-items: ${({ alignItems }) => alignItems};
 
-	@media screen and (max-width: 500px) {
-		width: 100vw;
-	}
+  @media screen and (max-width: 500px) {
+    width: 100vw;
+  }
 `;
 
 export const MobileSizeWrapper = styled(Wrapper)`
-	display: flex;
-	flex-direction: column;
-	width: 400px;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
 `;
 
 export const RoundedWrapper = styled(BaseFlexWrapper)`
-	border-radius: 40px;
-	flex-direction: ${(props) => props.flexDirection};
-	background-color: #131317;
-	width: ${(props) => props.width}px;
-	height: ${(props) => props.height}px;
-	position: relative;
+  border-radius: 40px;
+  flex-direction: ${(props) => props.flexDirection};
+  background-color: #131317;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  position: relative;
 
-	@media screen and (max-width: 500px) {
-		width: ${(props) => props.mWidth}px;
-		height: ${(props) => props.mHeight}px;
-	}
+  @media screen and (max-width: 500px) {
+    width: ${(props) => props.mWidth}px;
+    height: ${(props) => props.mHeight}px;
+  }
 `;
 
 export const InputWrapper = styled.input`
-	width: ${({ width }) => width};
-	height: ${({ height }) => height};
-	border-radius: 8px;
-	border: 1px solid #d0d0d0;
-	background-color: white;
-	padding-left: 0.5rem;
-	position: relative;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border-radius: 8px;
+  border: 1px solid #d0d0d0;
+  background-color: white;
+  padding-left: 0.5rem;
+  position: relative;
 `;
 
-export const ButtonWrapper = styled.div`
-	display: flex;
-	width: ${({ width }) => width};
-	height: ${({ height }) => height};
-	border-radius: 8px;
-	border: 1px solid #767676;
-	background-color: #ffffff;
-	text-align: center;
-	justify-content: center;
-	align-items: center;
-	font-weight: bold;
+export const ButtonWrapper = styled.button`
+  display: flex;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border-radius: 8px;
+  border: 1px solid #767676;
+  background-color: #ffffff;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  font-weight: bold;
 `;
 
 InputWrapper.defaultProps = {
-	width: "270px",
-	height: "52px",
+  width: "270px",
+  height: "52px",
 };
 
 ButtonWrapper.defaultProps = {
-	width: "100px",
-	height: "52px",
+  width: "100px",
+  height: "52px",
 };
 
 RoundedWrapper.defaultProps = {
-	flexDirection: "row",
+  flexDirection: "row",
 };
 
 Wrapper.defaultProps = {
-	color: "white",
-	alignItems: "center",
+  color: "white",
+  alignItems: "center",
 };
