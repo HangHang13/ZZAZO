4626946d5c6b68827b5a9c450538779364f1b8be
Hyung Ju <ghj6282@daum.net>
2022-09-12 15:50:02 +0900

    feat plan model 작성

--- Backend/plan/models.py
+++ Backend/plan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Card(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cards')
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    date = models.CharField(max_length=10)
+    appointed_time = models.CharField(max_length=10)
