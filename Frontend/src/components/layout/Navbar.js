53c235f3859137d94455bd323ce8e4c62f051d58
SungSoo <tjdtn6725@naver.com>
2022-09-15 07:11:49 +0900

    feat 네비바 ui 구현

--- Frontend/src/components/layout/Navbar.js
+++ Frontend/src/components/layout/Navbar.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Wrapper } from "../styled/Wrapper";
+import { IoIosMenu } from "react-icons/io";
 
 const Common = styled.div`
   display: flex;
   justify-content: space-between;
+  margin: 20px;
   background-color: rgba(192, 240, 176, 0.5);
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    background-color: rgba(192, 240, 176, 0.5);
+  }
 `;
 
 const Home = styled.a`
-  display : flex;
-  align-itmes:center;
-  font-size:2.5rem;
-  margin 1rem;
-  text-decoration: none;
-  color: black;
-  font-weight: bold;
+    display: flex;
+    align-items:center;
+    font-size: 30px;
+    margin 10px;
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+    @media screen and (max-width: 700px) {
+    display: none;
+    }
 `;
 
 const NavbarItemList = styled.div`
   display: flex;
-  align-itmes: center;
+  align-items: center;
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    align-items: flex-end;
+    display: ${({ menu }) => {
+      return menu === false ? "none" : "flex";
+    }};
+  }
 `;
 
 const NavItem = styled.a`
@@ -29,18 +44,40 @@ const NavItem = styled.a`
   color: black;
 `;
 
+const Menubar = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  position: absolute;
+  right: 32px;
+  height: 97px;
+  @media screen and (min-width: 700px) {
+    display: none;
+  }
+`;
+
 const Navbar = () => {
+  const [menu, setmenu] = useState(false);
+
   return (
     <Common>
       <Home href="/">ZZAZO</Home>
 
-      <NavbarItemList>
+      <NavbarItemList menu={menu}>
         <NavItem href="/">약속잡기</NavItem>
         <NavItem href="/">공유일정확인</NavItem>
         <NavItem href="/">마이페이지</NavItem>
         <NavItem href="/">누구님</NavItem>
         <NavItem href="/">로그아웃</NavItem>
       </NavbarItemList>
+      <Menubar
+        href="#"
+        onClick={() => {
+          setmenu(!menu);
+        }}
+      >
+        <IoIosMenu />
+      </Menubar>
     </Common>
   );
 };
