bfd59cf0bf8f5fdebec544d8fef883c3ecd6ad12
Hyung Ju <ghj6282@daum.net>
2022-09-14 11:26:06 +0900

    feat: place 모델 추가

--- Backend/place/models.py
+++ Backend/place/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=100)
+    Type = models.CharField(max_length=100)
+    regist = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    # appointed_card = models.ManyToManyField(Card, related_name='place')
