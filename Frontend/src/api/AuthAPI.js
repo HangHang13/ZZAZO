980c52482564b2ceeb45b9623ea146668c31334f
mingyu <mingyu1218@naver.com>
2022-09-15 14:31:18 +0900

    fix: Cross Origin 문제 해결 (사실 백엔드 몫임 ㅎ)

--- Frontend/src/api/AuthAPI.js
+++ Frontend/src/api/AuthAPI.js
@@ -1,4 +1,31 @@
 import { client } from "./../utils/client";
+
 const login = async (data) => {
-  const response = await client.post(`/users/login`, data);
+  const result = await client
+    .post(`/users/login`, data)
+    .then((response) => response.data)
+    .catch((error) => error.response);
+  return result;
 };
+
+const emailDuplicateCheck = async (data) => {
+  const result = await client
+    .get(`/users/checkemail/${data}/`)
+    .then((response) => response.data)
+    .catch((error) => error.response);
+  return result;
+};
+
+const nickNameDuplicateCheck = async (data) => {
+  const result = await client
+    .get(`/users/checkNickName/${data}/`)
+    .then((response) => response.data)
+    .catch((error) => error.response);
+  return result;
+};
+
+const emailConfirm = async (data) => {
+  alert(data);
+};
+
+export { login, emailDuplicateCheck, nickNameDuplicateCheck, emailConfirm };
