f5c6ddf5beef41a6ad8ef0454420839f480addcd
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2022-09-15 22:28:32 +0900

    feat:UpdateProfile 반응형웹CSS 수정

--- Frontend/src/api/mypage/MyPage.js
+++ Frontend/src/api/mypage/MyPage.js
@@ -3,7 +3,7 @@ import { client } from "./../../utils/client";
 //회원 본인 정보 조회
 const getUser = async (data) => {
   const result = await client
-    .get(`/users/me`, data)
+    .get(`/users/me/`, data)
     .then((response) => response.data)
     .catch((error) => error.response);
   return result;
@@ -12,8 +12,9 @@ const getUser = async (data) => {
 //회원 정보 수정
 const updateProfile = async (data) => {
   const result = await client
-    .put(`/users`, data)
+    .put(`/users/`, data)
     .then((response) => response.data)
     .catch((error) => error.response);
   return result;
 };
+export { getUser, updateProfile };
