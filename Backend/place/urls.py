5966badc23865812bf2fa6bc39366bde6ac7938d
Hyung Ju <ghj6282@daum.net>
2022-09-15 13:08:49 +0900

    feat: 리뷰 수정 및 삭제

--- Backend/place/urls.py
+++ Backend/place/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'place'
 urlpatterns = [
+    path('<str:place_id>/review/<str:review_id>', views.place_review_update_or_delete),
     path('<str:place_id>/review', views.place_review_create),
     path('<str:place_id>/', views.place_detail, name='detail'),
 ]
--- Backend/place/views.py
+++ Backend/place/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -23,23 +25,59 @@ def place_detail(request, place_id):
 
 @api_view(['POST'])
 def place_review_create(request, place_id):
-        serializer = ReviewCreateSerializer(data=request.data)
-        place = Place.objects.get(id = place_id)
-        if serializer.is_valid(raise_exception=True):
-            # serializer.save(user=request.user, place=place)
-            code = 200
-            message = "리뷰 생성"
-            res = {
-                "code": code,
-                "message": message,
-            }
-            return Response(res, status=status.HTTP_201_CREATED)
+    serializer = ReviewCreateSerializer(data=request.data)
+    place = Place.objects.get(id = place_id)
+    if serializer.is_valid(raise_exception=True):
+        # serializer.save(user=request.user, place=place)
+        code = 200
+        message = "리뷰 생성"
+        res = {
+            "code": code,
+            "message": message,
+        }
+        return Response(res, status=status.HTTP_201_CREATED)
+    
+    else:
+        code = 401
+        message = "리뷰 생성 실패"
+        res = {
+            "code": code,
+            "message": message,
+        }
+        return Response(res)
         
-        else:
-            code = 401
-            message = "리뷰 생성 실패"
+@api_view(['PUT', 'DELETE'])
+def place_review_update_or_delete(request, place_id, review_id):
+    review = get_object_or_404(Review, pk = review_id)
+    
+    def review_update():
+        if request.user == review.user:
+            serializer = ReviewCreateSerializer(instance=review, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                code = 200
+                message = "리뷰 수정"
+                res = {
+                    "code": code,
+                    "message": message,
+                }
+            return Response(res)
+
+    def review_delete():
+        if request.user == review.user:
+            review.delete()
+            code = 200
+            message = "리뷰 삭제"
             res = {
                 "code": code,
                 "message": message,
             }
             return Response(res)
+    
+    if request.method == 'PUT':
+        return review_update()
+    
+    elif request.method == 'DELETE':
+        return review_delete()
+
+    
