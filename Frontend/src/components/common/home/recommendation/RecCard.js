265879b695a1fea0c80cc367cd509f744561d768
mingyu <mingyu1218@naver.com>
2022-09-18 14:44:14 +0900

    fix: import 문제 해결, 메인 transiton 추가

--- Frontend/src/components/common/home/recommendation/RecCard.js
+++ Frontend/src/components/common/home/recommendation/RecCard.js
@@ -2,53 +2,54 @@ import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  &:hover {
-    transform: scale(1.1);
-  }
-  border-radius: 5px;
-  box-shadow: 2px 2px 2px grey;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  background: #fff;
-  margin: 2rem;
-  width: 13rem;
-  border: none;
-  flex-direction: column;
+	&:hover {
+		transform: scale(1.1);
+		transition: all 0.15s ease-in;
+	}
+	border-radius: 5px;
+	box-shadow: 2px 2px 2px grey;
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	background: #fff;
+	margin: 2rem;
+	width: 13rem;
+	border: none;
+	flex-direction: column;
 `;
 const CardImg = styled.img`
-  height: 10rem;
-  padding: 1rem;
-  display: flex;
-  src: ${({ src }) => src};
+	height: 10rem;
+	padding: 1rem;
+	display: flex;
+	src: ${({ src }) => src};
 `;
 const CardHeader = styled.div`
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin-bottom: 1rem;
+	font-weight: bold;
+	font-size: 1.2rem;
+	margin-bottom: 1rem;
 `;
 
 const CardAddress = styled.div`
-  font-weight: bold;
-  font-size: 0.7rem;
-  margin-bottom: 1rem;
+	font-weight: bold;
+	font-size: 0.7rem;
+	margin-bottom: 1rem;
 `;
 const CardTarget = styled.div`
-  font-weight: bold;
-  font-size: 0.7rem;
-  margin-bottom: 2rem;
+	font-weight: bold;
+	font-size: 0.7rem;
+	margin-bottom: 2rem;
 `;
 
 const RecCard = ({ src, text, address, target }) => {
-  return (
-    <Card>
-      <CardImg src={src}></CardImg>
-      <CardHeader>{text}</CardHeader>
+	return (
+		<Card>
+			<CardImg src={src}></CardImg>
+			<CardHeader>{text}</CardHeader>
 
-      <CardAddress>{address}</CardAddress>
-      <CardTarget>{target}</CardTarget>
-    </Card>
-  );
+			<CardAddress>{address}</CardAddress>
+			<CardTarget>{target}</CardTarget>
+		</Card>
+	);
 };
 
 export default RecCard;
