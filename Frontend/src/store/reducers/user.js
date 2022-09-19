304b2c01fed47ba6966773185d28f1a1b979e14a
mingyu <mingyu1218@naver.com>
2022-09-19 13:47:28 +0900

    fix: 마이페이지 회원 정보 변경 문제 해결

--- Frontend/src/store/reducers/user.js
+++ Frontend/src/store/reducers/user.js
@@ -1,29 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-	name: "user",
-	initialState: {
-		value: {
-			isLogin: false,
-			data: null,
-		},
-	},
-	reducers: {
-		storeLogin: (state, action) => {
-			state.value = action.payload;
-		},
-		storeLogout: (state, action) => {
-			state.value = {
-				isLogin: false,
-				data: null,
-			};
-			sessionStorage.removeItem("ACCESS_TOKEN");
-			sessionStorage.removeItem("REFRESH_TOKEN");
-			localStorage.removeItem("persist:root");
-			localStorage.removeItem("jwt");
-		},
-	},
+  name: "user",
+  initialState: {
+    value: {
+      isLogin: false,
+      data: null,
+    },
+  },
+  reducers: {
+    storeLogin: (state, action) => {
+      state.value = action.payload;
+    },
+    storeLogout: (state, action) => {
+      state.value = {
+        isLogin: false,
+        data: null,
+      };
+      sessionStorage.removeItem("ACCESS_TOKEN");
+      sessionStorage.removeItem("REFRESH_TOKEN");
+      localStorage.removeItem("persist:root");
+      localStorage.removeItem("jwt");
+    },
+    storeSetUserProfile: (state, action) => {
+      console.log(state.value);
+      console.log(action.payload);
+      state.value = action.payload;
+    },
+  },
 });
 
-export const { storeLogin, storeLogout } = userSlice.actions;
+export const { storeLogin, storeLogout, storeSetUserProfile } =
+  userSlice.actions;
 export default userSlice.reducer;
