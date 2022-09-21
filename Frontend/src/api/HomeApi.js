0a76a5d23d6db1ada651c5bdac7d5dbdd2226aaa
SungSoo <tjdtn6725@naver.com>
2022-09-22 06:52:00 +0900

    feat : 메인페이지 추천장소 api 호출 및 디자인 수정

---
+++ Frontend/src/api/HomeApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000/api/v1";
+
+const main = axios.create({
+  baseURL: BASE_URL,
+});
+
+// const getRec = async () => {
+//   const result = await main
+//     .get(`/home/`)
+//     .then((response) => {
+//       console.log(response.data.Place);
+//     })
+//     .catch((error) => error.response);
+// };
+
+const getRec = async () => {
+  const result = await main
+    .get(`/home/`)
+    .then((response) => response.data)
+    .catch((error) => error.response);
+
+  return result;
+};
+export { getRec };
