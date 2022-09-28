540d189aeb340920ad2a10bbe7381e6c1863e6c5
mingyu <mingyu1218@naver.com>
2022-09-28 10:06:21 +0900

    hotfix: API 요청 도메인 수정

--- Frontend/src/api/HomeApi.js
+++ Frontend/src/api/HomeApi.js
@@ -2,30 +2,12 @@ import axios from "axios";
 
 import { client } from "./../utils/client";
 
-
-const BASE_URL = "http://localhost/api/v1";
-// const BASE_URL = "http://j7b307.p.ssafy.io:8000/api/v1";
-
-const main = axios.create({
-  baseURL: BASE_URL,
-});
-
-// const getRec = async () => {
-//   const result = await main
-//     .get(`/home/`)
-//     .then((response) => {
-//       console.log(response.data.Place);
-//     })
-//     .catch((error) => error.response);
-// };
-
-
 const getRec = async () => {
-  const result = await client
-    .get(`/home`)
-    .then((response) => response.data)
-    .catch((error) => error.response);
+	const result = await client
+		.get(`/home`)
+		.then((response) => response.data)
+		.catch((error) => error.response);
 
-  return result;
+	return result;
 };
 export { getRec };
