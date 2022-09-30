2c583cb1b203659313b6c2b8f2d9551bb943c7a7
HangHang <dhkdwlsgod@naver.com>
2022-09-30 23:33:53 +0900

    fix:dockerfile

--- Backend/ZZAZO/wsgi.py
+++ Backend/ZZAZO/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZZAZO.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZZAZO.settings.base')
 
 application = get_wsgi_application()
