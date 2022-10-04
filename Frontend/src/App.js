29cc5bf857e3164d7d63e99d9be2251a71261cd3
mingyu <mingyu1218@naver.com>
2022-10-04 23:41:09 +0900

    fix: 회원가입 시 5세 이상만 가능하게 수정

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  Route,
-  Routes,
-  BrowserRouter as Router,
-  useLocation,
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router, useLocation } from "react-router-dom";
 import MyPage from "./pages/mypage/MyPage";
 import Home from "./pages/home/Home";
 import Signup from "./pages/auth/Signup";
@@ -26,59 +21,41 @@ import PlanMakeCard from "./pages/plan/PlanMakeCard";
 import PlanShare from "./pages/planshare/PlanShare";
 
 function App() {
-  // const location = useLocation();
+	const onHandleLogOut = () => {
+		dispatch(storeLogout());
+		navigate("/");
+	};
 
-  const onHandleLogOut = () => {
-    dispatch(storeLogout());
-    navigate("/");
-  };
+	useEffect(() => {
+		Kakao.init(process.env.REACT_APP_KAKAOLINK_API_KEY);
+	}, []);
 
-  const onHandleBrowserClose = (e) => {
-    e.preventDefault();
-    onHandleLogOut();
-  };
-  useEffect(() => {
-    (() => {
-      window.addEventListener("beforeunload", onHandleBrowserClose);
-    })();
-    Kakao.init(process.env.REACT_APP_KAKAOLINK_API_KEY);
-    return () => {
-      window.removeEventListener("beforeunload", onHandleBrowserClose);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/mypage"
-            element={<PrivateRoute component={<MyPage />} />}
-          >
-            <Route index element={<UpdateProfile />} />
-            <Route path="update/profile" element={<UpdateProfile />} />
-            <Route path="update/password" element={<UpdatePassword />} />
-            <Route path="delete/profile" element={<DeleteProfile />} />
-          </Route>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signupinterests" element={<SignupInterests />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/findid" element={<FindIdMain />} />
-          <Route path="/findid/result" element={<FindIdResult />} />
-          <Route path="/findpw" element={<FindPwMain />} />
-          <Route path="/plan" element={<PrivateRoute component={<Plan />} />} />
-          <Route
-            path="/planmakecard"
-            element={<PrivateRoute component={<PlanMakeCard />} />}
-          />
-          <Route path="/plancalendar" element={<PlanCalendar />} />
-          <Route path="/planshare/:cardId" element={<PlanShare />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/mypage" element={<PrivateRoute component={<MyPage />} />}>
+						<Route index element={<UpdateProfile />} />
+						<Route path="update/profile" element={<UpdateProfile />} />
+						<Route path="update/password" element={<UpdatePassword />} />
+						<Route path="delete/profile" element={<DeleteProfile />} />
+					</Route>
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/signupinterests" element={<SignupInterests />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/findid" element={<FindIdMain />} />
+					<Route path="/findid/result" element={<FindIdResult />} />
+					<Route path="/findpw" element={<FindPwMain />} />
+					<Route path="/plan" element={<PrivateRoute component={<Plan />} />} />
+					<Route path="/planmakecard" element={<PrivateRoute component={<PlanMakeCard />} />} />
+					<Route path="/plancalendar" element={<PlanCalendar />} />
+					<Route path="/planshare/:cardId" element={<PlanShare />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
