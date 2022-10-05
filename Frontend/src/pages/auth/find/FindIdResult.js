4a8e4a1099c47f9aba7b24845d7a2b03b711c880
mingyu <mingyu1218@naver.com>
2022-10-06 01:48:22 +0900

    fix: 아이디 찾기 버그 수정

--- Frontend/src/pages/auth/find/FindIdResult.js
+++ Frontend/src/pages/auth/find/FindIdResult.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import AuthButton from "../../../components/common/buttons/AuthButton";
 import Header from "../../../components/layout/Header";
 import { AuthWrapper, FindGuide, FindTitle, LogoImage } from "../../../components/styled/Auth";
 import { ColWrapper, Wrapper } from "../../../components/styled/Wrapper";
@@ -31,7 +30,15 @@ const FindIdResult = () => {
 						<LogoImage src="/assets/logo.png" />
 						<FindTitle>아이디 찾기</FindTitle>
 						<FindGuide>입력한 정보로 가입된 계정은 아래와 같습니다.</FindGuide>
-						<AuthWrapper>{state !== null ? state.userEmail : ""}</AuthWrapper>
+						<AuthWrapper>
+							{state !== null
+								? state.userEmails.map((item, index) => (
+										<div style={{ margin: "1rem" }} key={index}>
+											{item}
+										</div>
+								  ))
+								: ""}
+						</AuthWrapper>
 						<Button message="확 인" width="80%" color="white" bg="#80E080" borderColor="#80C0A0" clickEvent={moveHome}></Button>
 					</AuthWrapper>
 				</ColWrapper>
