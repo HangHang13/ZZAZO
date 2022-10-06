47e105e1c115126180ba10bb2e224c8b0338497b
mingyu <mingyu1218@naver.com>
2022-10-07 00:34:23 +0900

    refactor: console, 불필요한 주석 제거

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -4,21 +4,22 @@ import userReducer from "./reducers/user";
 import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import positionReducer from "./reducers/position";
+
 const reducers = combineReducers({
-  user: userReducer,
-  position: positionReducer,
+	user: userReducer,
+	position: positionReducer,
 });
 
 const persistConfig = {
-  key: "root",
-  storage,
+	key: "root",
+	storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== "production",
-  middleware: [thunk],
+	reducer: persistedReducer,
+	devTools: process.env.NODE_ENV !== "production",
+	middleware: [thunk],
 });
 export default store;
