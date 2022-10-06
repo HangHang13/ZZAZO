73e7fdae68862dd0f25de9fba1d3244f4b5ad81e
HangHang <dhkdwlsgod@naver.com>
2022-10-07 01:37:49 +0900

    fix:requiremntes

--- Backend/ZZAZO/locustfile.py
+++ Backend/ZZAZO/locustfile.py
@@ -1,18 +1,20 @@
 # locustfile.py
 from locust import HttpUser, task, between
 import json
+#테스트 locust
 class WebsiteTestUser(HttpUser):
      
     wait_time = between(1, 2.5)
     @task
     def my_task(self):
-        token ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY1MTEzMzM1LCJpYXQiOjE2NjUwNDEzMzUsImp0aSI6IjYwNGJiNzgxN2FiMzQ2M2VhZTQ1OGE5ZjM0OTYzMWJjIiwidXNlcl9pZCI6MzR9.wbwcvH2OvRDlP3TIE0zTnvjV_6FCFJSW3O9dyMkVKF4"
+        token =""
         
         headers = {'Content-Type':'application/json', 'Authorization' : 'Bearer ' + token}
         payload = {
             "longitude" : 127.29955512818,
             "latitude" : 36.3487861008275,
-            "radius" : 500
+            "radius" : 300
         }
         # "https://j7b307.p.ssafy.io/api/v1/place/recommend"
-        self.client.post("https://j7b307.p.ssafy.io/api/v1/place/recommend", headers=headers, data = json.dumps(payload))
+        # self.client.post("https://j7b307.p.ssafy.io/", headers=headers, data = json.dumps(payload))
+        self.client.get("https://j7b307.p.ssafy.io/")
