a6d9d37c44929df8bdb592526a79963cadaef39d
HangHang <dhkdwlsgod@naver.com>
2022-10-07 01:44:17 +0900

    fix: gunicorn

--- Backend/ZZAZO/locustfile.py
+++ Backend/ZZAZO/locustfile.py
@@ -1,7 +1,6 @@
 # locustfile.py
 from locust import HttpUser, task, between
 import json
-#테스트 locust
 class WebsiteTestUser(HttpUser):
      
     wait_time = between(1, 2.5)
@@ -13,8 +12,7 @@ class WebsiteTestUser(HttpUser):
         payload = {
             "longitude" : 127.29955512818,
             "latitude" : 36.3487861008275,
-            "radius" : 300
+            "radius" : 500
         }
         # "https://j7b307.p.ssafy.io/api/v1/place/recommend"
-        # self.client.post("https://j7b307.p.ssafy.io/", headers=headers, data = json.dumps(payload))
-        self.client.get("https://j7b307.p.ssafy.io/")
+        self.client.post("https://j7b307.p.ssafy.io/api/v1/place/recommend", headers=headers, data = json.dumps(payload))
